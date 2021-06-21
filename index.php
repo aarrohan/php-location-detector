398c5c1ae3d45205536b8c32c543d4ffce9249a7
Abdullah Al Reza Rohan <76879321+aarrohan@users.noreply.github.com>
2021-06-21 12:58:02 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+// PHP code to obtain country, city,  
+// continent, etc using IP Address 
+  
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$ipdat = @json_decode(file_get_contents( 
+    "http://www.geoplugin.net/json.gp?ip=" . $ip)); 
+   
+echo 'Country Name: ' . $ipdat->geoplugin_countryName . "\n"; 
+echo 'City Name: ' . $ipdat->geoplugin_city . "\n"; 
+echo 'Continent Name: ' . $ipdat->geoplugin_continentName . "\n"; 
+echo 'Latitude: ' . $ipdat->geoplugin_latitude . "\n"; 
+echo 'Longitude: ' . $ipdat->geoplugin_longitude . "\n"; 
+echo 'Currency Symbol: ' . $ipdat->geoplugin_currencySymbol . "\n"; 
+echo 'Currency Code: ' . $ipdat->geoplugin_currencyCode . "\n"; 
+echo 'Timezone: ' . $ipdat->geoplugin_timezone; 
+   
+?>
